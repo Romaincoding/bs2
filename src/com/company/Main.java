57344550e4b0908a4ed41c991d8d3d158ca09d05
Unknown <unknown@example.com>
2019-10-28 15:38:17 +0100

    Fonction tableau

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
-
 public class Main {
 
+    //Fonction tableau
+    static void  createTable() {
+        char tab[][] = new char[10][10];
+        System.out.println("  A B C D E F G H I J");
+        for (int row = 0;row<10;row++){
+            String affichage ="";
+            for(int col= 0;col<10;col++){
+                tab [col] [row] = '~';
+                affichage = affichage + tab [col][row] + ' ';
+            }
+            System.out.println(row + " "+ affichage);
+        }
+    }
+
     public static void main(String[] args) {
-	// write your code here
+
+        // write your code here
+        createTable();
+
     }
 }
