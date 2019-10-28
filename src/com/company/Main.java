8a3f4aa62454f64e45846943dbcc23169ab7e8e2
Romain <romain.dupuis@edu.itescia.fr>
2019-10-28 16:53:58 +0100

    Separate the functions displayTable and initTable

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,24 +1,53 @@
 package com.company;
+//import Math.*;
+
 public class Main {
 
-    //Fonction tableau
-    static void  createTable() {
-        char tab[][] = new char[10][10];
+    static void initTable(char tab[][]){
+
+        for (int row = 0;row<10;row++){
+            for(int col= 0;col<10;col++){
+                tab [col] [row] = '~';
+
+            }
+
+        }
+
+    }
+
+    //
+    static void displayTable(char tab[][]) {
+
         System.out.println("  A B C D E F G H I J");
         for (int row = 0;row<10;row++){
             String affichage ="";
             for(int col= 0;col<10;col++){
-                tab [col] [row] = '~';
+
                 affichage = affichage + tab [col][row] + ' ';
             }
             System.out.println(row + " "+ affichage);
         }
+
     }
 
     public static void main(String[] args) {
 
-        // write your code here
-        createTable();
+        char tableau[][] = new char[10][10];
+
+        System.out.println("Ahoy Matey"); // write your code here
+        initTable(tableau);
+        displayTable(tableau);
+
+        // generate ships in the table
+         String shipOne = "S S S S S";
+
+
+        /* for(int z=0;z<10000;z++){
+             int indRow = (int)(Math.random()*10);
+             System.out.println(indRow);
+
+
+         }*/
 
     }
 }
