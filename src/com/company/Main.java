c265ebbc35bf7f5b5dd853d37326b98d1f4372ba
Romain <romain.dupuis@edu.itescia.fr>
2019-10-29 09:06:35 +0100

    Ship generator

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 
 public class Main {
 
+
     static void initTable(char tab[][]){
 
         for (int row = 0;row<10;row++){
@@ -28,18 +29,54 @@ public class Main {
             System.out.println(row + " "+ affichage);
         }
 
+
+
+
+    }
+
+    static void generateShip(int size, char tab [][]){
+        int randomPointRow = (int) (Math.random()*10);
+        int randomPointCol = (int) (Math.random()*10);
+
+        tab [randomPointCol] [randomPointRow]= 'S';
+        if (size == 2){
+            if (randomPointCol != 0) {
+                tab[randomPointCol][randomPointRow] = 'S';
+                tab[randomPointCol - 1][randomPointRow] = 'S';
+            }else{
+                tab[randomPointCol][randomPointRow] = 'S';
+                tab[randomPointCol + 1][randomPointRow] = 'S';
+
+            }
+        }
+
+    }
+    static boolean isFree (int size, int col, int row, char tab [][]){
+        // size = 2
+        // col = 1
+        // row = 4
+        for(int i=0;i<size;i++) {
+            if (tab[row][col - i] == 'S') {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
 
         char tableau[][] = new char[10][10];
 
-        System.out.println("Ahoy Matey"); // write your code here
+        System.out.println("AHOY MATEY"); // write your code here
         initTable(tableau);
         displayTable(tableau);
 
         // generate ships in the table
-         String shipOne = "S S S S S";
+        generateShip(2,tableau);
+        generateShip(1,tableau);
+
+        displayTable(tableau);
+
 
 
         /* for(int z=0;z<10000;z++){
